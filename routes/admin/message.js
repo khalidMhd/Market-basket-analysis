9b28f6b9593a76a3433e01babf5479e5a70cb72c
khalidmhd <khalidmhd1137@gmail.com>
2022-03-07 10:53:06 +0500

    pagnation

--- routes/admin/message.js
+++ routes/admin/message.js
@@ -4,9 +4,10 @@ var multer = require('multer')
 const messageModel = require('../../models/message')
 const userIdFromJWT = require('../../middleware/userIdJWT');
 const adminLoginRequire = require('../../middleware/adminLoginRequire')
+const paginatedResults = require('../../middleware/pagination')
 
-router.get('/message',adminLoginRequire, (req, res) => {
-    messageModel.find().sort({"createdAt": -1}).populate('user', 'name email').then(data => {
+router.get('/message', adminLoginRequire, (req, res) => {
+    messageModel.find().sort({ "createdAt": -1 }).populate('user', 'name email').then(data => {
         res.status(200).json(data)
     }).catch((err) => {
         res.status(422).json({ message: "Something went wrong!" })
@@ -14,7 +15,7 @@ router.get('/message',adminLoginRequire, (req, res) => {
 })
 
 // confirm premium
-router.post("/confirm-msg-read/:id",adminLoginRequire, async (req, res) => {
+router.post("/confirm-msg-read/:id", adminLoginRequire, async (req, res) => {
     const id = req.params.id
     const readMessage = await messageModel.findOne({ _id: id })
     if (readMessage) {
